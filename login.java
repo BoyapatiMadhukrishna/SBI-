6b860e67c9905526090df61dddbfa83b9f9dc4ab
BoyapatiMadhukrishna <madhukrishnaboyapati@gmail.com>
2023-06-23 14:53:50 +0000

    added changes by Friend

--- login.java
+++ login.java
@@ -1,4 +1,8 @@
 import java.io.BufferedReader;
+
+i am changing the login page detals
+because the login details wrong enter
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Test {
